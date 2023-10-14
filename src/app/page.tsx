b8c6a434cf7e3081af01a5b740296b5982532e8e
crayonzgrim <crayonzgrim@gmail.com>
2023-10-14 19:53:08 +0900

    :tada: Init project

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="">
+      <div className="text-red-800">Hello</div>
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{ts,tsx,mdx}",
+    "./src/components/**/*.{ts,tsx,mdx}",
+    "./src/app/**/*.{ts,tsx,mdx}",
+  ],
+  theme: {},
+  plugins: [],
+};
+export default config;
