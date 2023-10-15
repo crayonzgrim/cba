19669a8b4d9c667beec369df967eac57586d943f
crayonzgrim <crayonzgrim@gmail.com>
2023-10-15 09:28:21 +0900

    :hammer: Add Filtered post menu

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,10 @@
+import { getAllPosts } from "@/service/posts";
 import React from "react";
+import FilterablePosts from "../components/FilterablePosts";
 
-export default function PostsPage() {
-  return <p>Post Page</p>;
+export default async function PostsPage() {
+  const posts = await getAllPosts();
+  const categories = [...new Set(posts.map((post) => post.category))];
+
+  return <FilterablePosts posts={posts} categories={categories} />;
 }
